eadb9e4f59515309705f69fd80dae09605bbe2a9
Liam Appleton <liamappleton97@outlook.com>
2025-05-09 19:05:41 +0100

    creating Car class

---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+namespace rentalapp;
+
+public class Car
+{
+    private string Make { get; set; }
+    private string Model { get; set; }
+    private int Year { get; set; }
+    private string Colour { get; set; }
+    public Car(string make, string model, int year, string colour)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Colour = colour;
+    }
+}
