50feea1cdf2d4be5da8a1de95c272c19836d68f3
Liam Appleton <liamappleton97@outlook.com>
2025-05-09 19:12:20 +0100

    adding DisplayDetails method to Car

--- Models/Car.cs
+++ Models/Car.cs
@@ -6,11 +6,17 @@ public class Car
     private string Model { get; set; }
     private int Year { get; set; }
     private string Colour { get; set; }
+
     public Car(string make, string model, int year, string colour)
     {
         Make = make;
         Model = model;
         Year = year;
         Colour = colour;
+    }
+
+    public override string ToString()
+    {
+        return $"{Make} | {Model} ({Colour}) [{Year}]";
     }
 }
