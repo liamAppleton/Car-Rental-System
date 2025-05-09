160f5c4fe58292ef1aef8b558a2eed3debe42c39
Liam Appleton <liamappleton97@outlook.com>
2025-05-09 19:48:42 +0100

    implementing IRentalItem in Car

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,12 +1,13 @@
 namespace rentalapp;
 
-public class Car : IInfoDisplay
+public class Car : IInfoDisplay, IRentalItem
 {
     private int CarId { get; set; }
     private string Make { get; set; }
     private string Model { get; set; }
     private int Year { get; set; }
     private string Colour { get; set; }
+    public bool IsRented { get; private set; } = false;
 
     public Car(int carId, string make, string model, int year, string colour)
     {
@@ -20,5 +21,25 @@ public class Car : IInfoDisplay
     public void DisplayDetails()
     {
         Console.WriteLine($"{Make} | {Model} ({Colour}) [{Year}]");
+    }
+
+    public void Rent()
+    {
+        if (!IsRented)
+        {
+            IsRented = true;
+            Console.WriteLine($"{CarId} ({Colour} {Make} {Model}) is now rented.");
+        }
+        else Console.WriteLine("Car is already being rented.");
+    }
+
+    public void Return()
+    {
+        if (IsRented)
+        {
+            IsRented = false;
+            Console.WriteLine($"{CarId} ({Colour} {Make} {Model}) is now returned.");
+        }
+        else Console.WriteLine("Car is already returned.");
     }
 }
