f30aaa5da14543b2c3e018d4b16c3693104f1a99
Liam Appleton <liamappleton97@outlook.com>
2025-05-10 15:50:27 +0100

    replacing CarId and BikeId with corresponding type objects

--- Models/Rental.cs
+++ Models/Rental.cs
@@ -4,17 +4,17 @@ public class Rental
 {
     public int RentalId { get; private set; }
     public int CustomerId { get; private set; }
-    public int? CarId { get; private set; }
-    public int? BikeId { get; private set; }
+    public Car? Car { get; private set; }
+    public Bike? Bike { get; private set; }
     public DateOnly RentalStartDate { get; private set; }
     public DateOnly RentalEndDate { get; private set; }
 
-    public Rental(int rentalId, int customerId, int carId, int bikeId, DateOnly rentalStartDate, DateOnly rentalEndDate)
+    public Rental(int rentalId, int customerId, Car car, Bike bike, DateOnly rentalStartDate, DateOnly rentalEndDate)
     {
         RentalId = rentalId;
         CustomerId = customerId;
-        CarId = carId;
-        BikeId = bikeId;
+        Car = car;
+        Bike = bike;
         RentalStartDate = rentalStartDate;
         RentalEndDate = rentalEndDate;
     }
