1b9348da5b2c210501adf484baae3b5df7fc8c28
Liam Appleton <liamappleton97@outlook.com>
2025-05-11 11:52:45 +0100

    creating VehicleQuery class

---
+++ Services/VehicleQuery.cs
@@ -0,0 +1,73 @@
+using Microsoft.VisualBasic;
+
+namespace rentalapp;
+
+public class VehicleQuery<T> where T : class, IRentalItem, IInfoDisplay
+{
+    private RentalManagement<T> _rentalManagement;
+
+    public VehicleQuery(RentalManagement<T> rentalManagement)
+    {
+        _rentalManagement = rentalManagement;
+    }
+
+    public List<Car> GetCurrentlyRentedCars()
+    {
+        return _rentalManagement.Cars
+            .Where(car => car.IsRented)
+            .ToList();
+    }
+
+    public List<Bike> GetCurrentlyRentedBikes()
+    {
+        return _rentalManagement.Bikes
+            .Where(bike => bike.IsRented)
+            .ToList();
+    }
+
+    public List<Car> GetCurrentlyAvailableCars()
+    {
+        return _rentalManagement.Cars
+            .Where(car => !car.IsRented)
+            .ToList();
+    }
+
+    public List<Bike> GetCurrentlyAvailableBikes()
+    {
+        return _rentalManagement.Bikes
+            .Where(bike => !bike.IsRented)
+            .ToList();
+    }
+
+    public List<Car> GetCarsByColour(string colour)
+    {
+        try
+        {
+            return _rentalManagement.Cars
+            .Where(car => car.Colour == colour)
+            .ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"No cars of colour: {colour} in database");
+            Console.WriteLine($"Error: {ex}");
+            return new List<Car>();
+        }
+    }
+
+    public List<Bike> GetBikesByColour(string colour)
+    {
+        try
+        {
+            return _rentalManagement.Bikes
+            .Where(bike => bike.Colour == colour)
+            .ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"No bikes of colour: {colour} in database");
+            Console.WriteLine($"Error: {ex}");
+            return new List<Bike>();
+        }
+    }
+}
