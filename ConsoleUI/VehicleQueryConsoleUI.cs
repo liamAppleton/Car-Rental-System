6e2789a6d7cbf34968d3710769b3f97a85ef0e89
Liam Appleton <liamappleton97@outlook.com>
2025-05-13 12:41:59 +0100

    adding rows to tables

--- ConsoleUI/VehicleQueryConsoleUI.cs
+++ ConsoleUI/VehicleQueryConsoleUI.cs
@@ -14,11 +14,31 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
     public void DisplayAllCars()
     {
         var table = new Table();
-        table.AddColumn("Cars");
+        table.AddColumn("Make");
+        table.AddColumn("Model");
+        table.AddColumn("Colour");
+        table.AddColumn("Year");
+        table.AddColumn("IsRented");
+        table.AddColumn("CarId");
 
         foreach (Car car in _rentalManagement.Cars)
         {
-            table.AddRow(car.GetVehicleDetails());
+            table.AddRow(car.Make, car.Model, car.Colour, car.Year.ToString(), car.IsRented.ToString(), car.CarId.ToString());
+        }
+
+        AnsiConsole.Write(table);
+    }
+
+    public void DisplayAllBikes()
+    {
+        var table = new Table();
+        table.AddColumn("Colour");
+        table.AddColumn("IsRented?");
+        table.AddColumn("BikeId");
+
+        foreach (Bike bike in _rentalManagement.Bikes)
+        {
+            table.AddRow(bike.Colour, bike.IsRented.ToString(), bike.BikeId.ToString());
         }
 
         AnsiConsole.Write(table);
