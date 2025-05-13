e8c2b0cbd2938ea3de03da0f5c5de6764a426447
Liam Appleton <liamappleton97@outlook.com>
2025-05-13 12:31:56 +0100

    adding DisplayAllCars to VehicleQueryConsoleUI

--- ConsoleUI/VehicleQueryConsoleUI.cs
+++ ConsoleUI/VehicleQueryConsoleUI.cs
@@ -1,3 +1,5 @@
+using Spectre.Console;
+
 namespace rentalapp;
 
 public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
@@ -7,6 +9,19 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
     public VehicleQueryConsoleUI(RentalManagement<T> rentalManagement)
     {
         _rentalManagement = rentalManagement;
+    }
+
+    public void DisplayAllCars()
+    {
+        var table = new Table();
+        table.AddColumn("Cars");
+
+        foreach (Car car in _rentalManagement.Cars)
+        {
+            table.AddRow(car.GetVehicleDetails());
+        }
+
+        AnsiConsole.Write(table);
     }
 
 
