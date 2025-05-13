392133be95ef3a6eaa4781f26dbe23834d98dbd2
Liam Appleton <liamappleton97@outlook.com>
2025-05-13 10:23:39 +0100

    adding AddInputRental method to RentalConsoleUI

--- ConsoleUI/RentalConsoleUI.cs
+++ ConsoleUI/RentalConsoleUI.cs
@@ -1,4 +1,5 @@
 using Spectre.Console;
+using System.Text.RegularExpressions;
 
 namespace rentalapp;
 
@@ -13,7 +14,63 @@ public class RentalConsoleUI<T> where T : class, IRentalItem
         _customerManagement = customerManagement;
     }
 
-    public DisplayAllRentalVehicles()
+    public void AddInputRental()
+    {
+        string[] vehiclesDetails = _rentalManagement.Cars.Cast<IRentalItem>()
+        .Concat(_rentalManagement.Bikes.Cast<IRentalItem>())
+        .Where(vehicle => !vehicle.IsRented)
+        .Select(vehicle => vehicle.GetVehicleDetails())
+        .ToArray();
+
+        var vehicle = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("Select a vehicle to rent: ")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to view more vehicles)[/]")
+            .AddChoices(vehiclesDetails)
+        );
+
+        Regex id = new Regex(@"- ([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$");
+        Match idMatch = id.Match(vehicle);
+
+        Regex carType = new Regex(@"\[CAR\]");
+        Match carMatch = carType.Match(vehicle);
+
+        Car? car = null;
+        Bike? bike = null;
+
+        if (carMatch.Success)
+        {
+            car = _rentalManagement.Cars
+                .FirstOrDefault(c => c.CarId == Guid.Parse(idMatch.Groups[1].Value));
+        }
+        else
+        {
+            bike = _rentalManagement.Bikes
+                .FirstOrDefault(b => b.BikeId == Guid.Parse(idMatch.Groups[0].Value));
+        }
+
+        var customerName = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title($"Select a customer to rent [{vehicle}]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to view more customers)[/]")
+            .AddChoices(_customerManagement.Customers
+                .Select(c => c.Name)
+                .ToArray())
+        );
+
+        Guid customerId = _customerManagement.Customers
+            .Where(c => c.Name == customerName)
+            .Select(c => c.CustomerId)
+            .FirstOrDefault();
+
+        Guid rentalId = Guid.NewGuid();
+
+        _rentalManagement.AddVehicle(new Rental(rentalId, customerId, car, bike));
+    }
+
+    public void DisplayAllRentalVehicles()
     {
         if (_rentalManagement.RentedVehicles.Count == 0)
         {
