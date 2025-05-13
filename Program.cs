31111e38a38cb636e4783f7c0a95a958c4063156
Liam Appleton <liamappleton97@outlook.com>
2025-05-13 11:08:50 +0100

    instantiating objects in Main

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            DataSeeder dataSeeder = new DataSeeder();
+            List<Car> cars = dataSeeder.GetCars();
+            List<Bike> bikes = dataSeeder.GetBikes();
+            List<Customer> customers = dataSeeder.GetCustomers();
 
+            EmailNotification emailNotification = new EmailNotification();
+
+            CustomerManagement customerManagement = new CustomerManagement(customers);
+            RentalManagement<IRentalItem> rentalManagement = new RentalManagement<IRentalItem>(new List<Rental>(), cars, bikes, emailNotification);
         }
     }
 }
