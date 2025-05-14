a53fed57973e11324e9fc444f8bae88698e66e06
Liam Appleton <liamappleton97@outlook.com>
2025-05-14 09:28:28 +0100

    adding RentedBy column to each table

--- ConsoleUI/VehicleQueryConsoleUI.cs
+++ ConsoleUI/VehicleQueryConsoleUI.cs
@@ -5,10 +5,12 @@ namespace rentalapp;
 public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
 {
     private RentalManagement<T> _rentalManagement;
+    private CustomerManagement _customerManagement;
 
-    public VehicleQueryConsoleUI(RentalManagement<T> rentalManagement)
+    public VehicleQueryConsoleUI(RentalManagement<T> rentalManagement, CustomerManagement customerManagement)
     {
         _rentalManagement = rentalManagement;
+        _customerManagement = customerManagement;
     }
 
     private Table InitialiseCarTable()
@@ -19,6 +21,7 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
         table.AddColumn("Colour");
         table.AddColumn("Year");
         table.AddColumn("IsRented");
+        table.AddColumn("RentedBy");
         table.AddColumn("CarId");
         return table;
     }
@@ -28,17 +31,38 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
         var table = new Table();
         table.AddColumn("Colour");
         table.AddColumn("IsRented");
+        table.AddColumn("RentedBy");
         table.AddColumn("BikeId");
         return table;
+    }
+
+    private string? GetRentingCustomer(string id, string type)
+    {
+        return _rentalManagement.RentedVehicles.Join(
+            _customerManagement.Customers,
+            rental => rental.CustomerId,
+            customer => customer.CustomerId,
+            (rental, customer) => new { rental, customer })
+            .Where(rentalCustomer => (type == "CAR" ?
+            rentalCustomer.rental.Car.CarId.ToString() :
+            rentalCustomer.rental.Bike.BikeId.ToString())
+            == id)
+            .Select(rentalCustomer => rentalCustomer.customer.Name)
+            .FirstOrDefault() ?? "N/A";
     }
 
     public void DisplayAllCars()
     {
         var table = InitialiseCarTable();
-
         foreach (Car car in _rentalManagement.Cars)
         {
-            table.AddRow(car.Make, car.Model, car.Colour, car.Year.ToString(), car.IsRented.ToString(), car.CarId.ToString());
+            table.AddRow(car.Make,
+            car.Model,
+            car.Colour,
+            car.Year.ToString(),
+            car.IsRented.ToString(),
+            GetRentingCustomer(car.CarId.ToString(), "CAR"),
+            car.CarId.ToString());
         }
 
         AnsiConsole.MarkupLine("\n[blue bold]Cars[/]");
@@ -52,7 +76,10 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
 
         foreach (Bike bike in _rentalManagement.Bikes)
         {
-            table.AddRow(bike.Colour, bike.IsRented.ToString(), bike.BikeId.ToString());
+            table.AddRow(bike.Colour,
+            bike.IsRented.ToString(),
+            GetRentingCustomer(bike.BikeId.ToString(), "BIKE"),
+            bike.BikeId.ToString());
         }
 
         AnsiConsole.MarkupLine("\n[yellow bold]Bikes[/]");
@@ -63,13 +90,20 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
     public void DisplayCarsByRentalStatus(bool isRented)
     {
         var table = InitialiseCarTable();
+
         List<Car> rentedCars = _rentalManagement.Cars
             .Where(car => isRented ? car.IsRented : !car.IsRented)
             .ToList();
 
         foreach (Car car in rentedCars)
         {
-            table.AddRow(car.Make, car.Model, car.Colour, car.Year.ToString(), car.IsRented.ToString(), car.CarId.ToString());
+            table.AddRow(car.Make,
+            car.Model,
+            car.Colour,
+            car.Year.ToString(),
+            car.IsRented.ToString(),
+            GetRentingCustomer(car.CarId.ToString(), "CAR"),
+            car.CarId.ToString());
         }
 
         AnsiConsole.MarkupLine($"\n[blue bold]{(isRented ? "Rented" : "Available")} Cars[/]");
@@ -86,7 +120,10 @@ public class VehicleQueryConsoleUI<T> where T : class, IRentalItem
 
         foreach (Bike bike in rentedBikes)
         {
-            table.AddRow(bike.Colour, bike.IsRented.ToString(), bike.BikeId.ToString());
+            table.AddRow(bike.Colour,
+            bike.IsRented.ToString(),
+            GetRentingCustomer(bike.BikeId.ToString(), "BIKE"),
+            bike.BikeId.ToString());
         }
 
         AnsiConsole.MarkupLine($"\n[red bold]{(isRented ? "Rented" : "Available")} Bikes[/]");
